6e1bd18847c29753b870c4562f583acd3c972f6a
Cesmac <cesmac@academico.cesmac.edu.br>
2019-02-28 20:32:57 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'criar-conta', loadChildren: './criar-conta/criar-conta.module#CriarContaPageModule' },  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },
+  { path: 'criar-conta', loadChildren: './criar-conta/criar-conta.module#CriarContaPageModule' },
+  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },
 
 ];
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MenuController, ToastController, AlertController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  msg = "";
+  formulario: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder, public toastController: ToastController, private router: Router, private navCtrl:NavController) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
+  clicou() {
+    if (this.formulario.valid && 
+        this.formulario.get('email').value == "teste@teste.com" &&
+        this.formulario.get('senha').value == "123456") {
+          this.router.navigateByUrl('/inicio');
+    } else {  
+      this.chamar();
+  }
+}
 
+async chamar() {
+  const toast = await this.toastController.create({
+    message: 'Usuario Não Cadastrado',
+    duration: 2000
+  });
+  toast.present();
+}
+ 
 }
